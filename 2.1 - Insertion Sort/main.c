911060769e2680d4f088343e76c0ad249437c292
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-09-14 18:24:26 +0200

    ADD: Inverted Insertion sort

--- 2.1 - Insertion Sort/main.c
+++ 2.1 - Insertion Sort/main.c
@@ -4,6 +4,7 @@
 int main(void) {
     int unorderedLength;
     int unorderedList[] = {5, 2, 4, 6, 1, 3};
+    int invertThisList[] = {1, 5, 2, 9, 4, 7};
     int key, previousIndex, currentItem;
 
     for (int i = 1; i < 6; i++) 
@@ -21,8 +22,27 @@ int main(void) {
         }
     }
 
+    for (int i = 1; i < 6; i++) {
+        currentItem = invertThisList[i];
+        previousIndex = i - 1;
+
+        while(previousIndex >= 0 && invertThisList[previousIndex] < currentItem) {
+            invertThisList[previousIndex + 1] = invertThisList[previousIndex];
+            previousIndex = previousIndex - 1;
+            invertThisList[previousIndex + 1] = currentItem;
+        }
+
+    }
+
     for (int i = 0; i < 6; i++) {
         printf("%d\n", unorderedList[i]);
     }
+
+    printf("\n");
+
+    for (int i = 0; i < 6; i++) {
+        printf("%d\n", invertThisList[i]);
+    }
+    
     return 0;
 }
